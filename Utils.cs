e50e44f80c1c47010545e0b3a78300b9d441b617
Richard Harkins <rharkins111@gmail.com>
2016-09-23 17:20:29 -0600

    Initial commit

---
+++ Utils.cs
@@ -0,0 +1,28 @@
+﻿/*
+
+automation challenge 8 (Crawler and image verification):
+Extension of the crawler challenge.  As you navigate through the site, find every broken image.  
+
+Go to https://www.skiutah.com/
+
+Verify that each reference to an image displays the image correctly and not a dead image.  Log every page that has a broken image to a text file.  
+
+*/
+
+namespace Automation_Code_Challenge_8_CSharp
+{
+    [TestClass]
+    class Utils : SeleniumWebdriverBaseClass
+    {
+        [TestMethod]
+        // This method is the main method for launching tests
+        public void TestLauncher()
+        {
+            // Verify Page Title
+            verifyPageTitle(URL, Expected_Value);
+            // Visit every page of the skiutah.com domain once, compiling a list of all unique images on the skiutah.com domain
+            // Log every broken image, along with the page where that image is located
+            webCrawler();
+        }
+    }
+}
